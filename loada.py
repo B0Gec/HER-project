5415d5df3d76abacb2931df2c890a6bc740353ec
bogec <bostogec@gmail.com>
2023-08-29 15:19:43 +0200

    some results after introducing precision parameter

--- loada.py
+++ loada.py
@@ -1,135 +1,158 @@
-import pandas as pd
-import numpy as np
-import os
+# 1.) loading txt as pandas dataframe
 
-dir = 'data' + os.sep
-fnamei = 'NEG_profil_Ni_VI_AE.txt'
-fnameiec = 'NEG_profil_Ni_VI_AE-edited.txt'
-fnameie = 'NEG_profil_Ni_VI_AEeh.txt'
-fnameie = 'NEG_profil_Ni_VI_AE-et.txt'
-fnameie = 'NEG_profil_Ni_VI_AE-ecom.txt'
-fnameii = 'NEG_profil_Ni_VII_AE.txt'
-fnameiii = 'NEG_profil_Ni_VIII_AE.txt'
-import io
-
+# import pandas as pd
+# import numpy as np
+# import os
+#
+# dir = 'data' + os.sep
+# fnamei = 'NEG_profil_Ni_VI_AE.txt'
+# fnameiec = 'NEG_profil_Ni_VI_AE-edited.txt'
+# fnameie = 'NEG_profil_Ni_VI_AEeh.txt'
+# fnameie = 'NEG_profil_Ni_VI_AE-et.txt'
+# fnameie = 'NEG_profil_Ni_VI_AE-ecom.txt'
+# fnameii = 'NEG_profil_Ni_VII_AE.txt'
+# fnameiii = 'NEG_profil_Ni_VIII_AE.txt'
+# import io
+#
+# # f = open(dir + fnamei, 'r')
+# # data = f.read()
+# # data = io.StringIO()
+# # f.close
+# # data = io.StringIO()
+#
+# print(dir + fnamei)
 # f = open(dir + fnamei, 'r')
-# data = f.read()
-# data = io.StringIO()
-# f.close
-# data = io.StringIO()
-
-print(dir + fnamei)
-f = open(dir + fnamei, 'r')
-nline = 2
-line = [f.readline() for i in range(nline+1)][nline]
-cols = ['t'] + line.split('\t')[1:-1]
-print(cols)
-f.close()
-print(len(cols))
-# 1/0
-# df = pd.read_csv(dir + fnamei, sep='\t', header=0, index_col=0)
-# df = pd.read_csv(dir + fnamei, sep='\t')
-df = pd.read_csv(dir + fnamei, comment='#', sep='\t')
-df = df.iloc[:, :-1]
-print(df)
-# 1/0
+# nline = 2
+# line = [f.readline() for i in range(nline+1)][nline]
+# cols = ['t'] + line.split('\t')[1:-1]
+# print(cols)
+# f.close()
+# print(len(cols))
+# # 1/0
+# # df = pd.read_csv(dir + fnamei, sep='\t', header=0, index_col=0)
+# # df = pd.read_csv(dir + fnamei, sep='\t')
+# df = pd.read_csv(dir + fnamei, comment='#', sep='\t')
+# df = df.iloc[:, :-1]
+# print(df)
+# # 1/0
+# # df = pd.read_csv(dir + fnameie, comment='#')
 # df = pd.read_csv(dir + fnameie, comment='#')
-df = pd.read_csv(dir + fnameie, comment='#')
-df = pd.read_csv(dir + fnameie, comment='#', sep='\t', usecols=[0,1,2,3,4,5,6,7,8,9,10,11,12,13])
-df = pd.read_csv(dir + fnameie, comment='#', sep='\t', usecols=[i for i in range(39)])
-print(df)
-print('a')
-# 1/0
-
-df = pd.read_csv(dir + fnameiec)
-df = pd.read_csv(dir + fnameiec, sep='\t')
-df = pd.read_csv(dir + fnameie, comment='#', sep='\t', usecols=[i for i in range(40)])
-print(df)
-# 1/0
+# df = pd.read_csv(dir + fnameie, comment='#', sep='\t', usecols=[0,1,2,3,4,5,6,7,8,9,10,11,12,13])
 # df = pd.read_csv(dir + fnameie, comment='#', sep='\t', usecols=[i for i in range(39)])
-print(len(df.columns), df.columns)
+# print(df)
+# print('a')
+# # 1/0
+#
+# df = pd.read_csv(dir + fnameiec)
+# df = pd.read_csv(dir + fnameiec, sep='\t')
+# df = pd.read_csv(dir + fnameie, comment='#', sep='\t', usecols=[i for i in range(40)])
+# print(df)
+# # 1/0
+# # df = pd.read_csv(dir + fnameie, comment='#', sep='\t', usecols=[i for i in range(39)])
+# print(len(df.columns), df.columns)
+#
+# def loaded(fname):
+#     # df_cut = df[1:]
+#     f = open(fname, 'r')
+#     nline = 2
+#     line = [f.readline() for i in range(nline + 1)][nline]
+#     cols = ['t'] + line.split('\t')[1:-1]
+#     f.close()
+#     # df = pd.read_csv(fname, comment='#', sep='\t', header=0)
+#     # df = pd.read_csv(fname, sep='\t', skiprows=5, header=None, names=cols)  # not working
+#     # df = pd.read_csv(fname, sep='\t', skiprows=5, header=0, names=cols)  # not working
+#     df = pd.read_csv(fname, sep='\t', skiprows=5, header=None)
+#     df.columns = cols + ['\t']
+#     df = df.iloc[:, :-1]
+#
+#
+#     return df, cols
+#
+#
+# print('\n'*5)
+# saved_cols = []
+# for fname in [fnamei, fnameii, fnameiii]:
+#     df, cols = loaded(dir + fnamei)
+#     print(f'starting loading {fname}')
+#     print(df)
+#     print(len(cols), cols)
+#     print(saved_cols == cols)
+#     saved_cols = cols
+#     print('end loading')
+# # 1/0
+#
+# # df = pd.read_csv(dir + fnamei)
+# # df = pd.read_csv(dir + fnameie, header=[0,1])
+# # df = pd.read_csv(dir + fnameie, sep='\t')
+# # df = pd.read_csv(dir + fnameie, sep='\t')
+# print(df.columns)
+#
+# print(df)
+#
+# df_cut = df[1:]
+# print(df_cut)
+# a = np.array(df)
+#
+# print(len(df.index))
+# # 1/0
+# # for row in df_cut:
+# #     print(row)
+# print(df[1:2])
+# print('df0')
+#
+# # print(a[1])
+#
+#
+# # for i in df:
+# #     print(i)
+#
+# def check_tot(df):
+#     """check tot"""
+#
+#     print('inside check tot')
+#
+#     print(len(df.index))
+#     count = 0
+#     for i in range(0, len(df.index)):
+#         # row = df[i:i+1]
+#         # print(i, row)
+#         row = np.array(df[i:i+1])[0]
+#         # row = list(df[i:i+1])
+#         # print(i, row)
+#
+#         # print([i for i in row])
+#         # print(row[1], 'data and sum', sum(row[2:]), sum(row[2:])*2, row[1:])
+#         print(row[1], 'data and sum', row[1]-sum(row[2:]))
+#         if row[1] < sum(row[2:]):
+#             count += 1
+#             print('\n'*5, 'count', count)
+#         # print(row[1], 'sum', sum(row[2:]), sum(row[2:])*2,)
+#         # print(row[1], 'explain', [sum(row[2:i]) for i in range(2+1, len(row))])
+#         # break
+#     print(count, 'count')
+#
+#     return
+#
+# check_tot(df_cut)
+#
 
-def loaded(fname):
-    # df_cut = df[1:]
-    f = open(fname, 'r')
-    nline = 2
-    line = [f.readline() for i in range(nline + 1)][nline]
-    cols = ['t'] + line.split('\t')[1:-1]
-    f.close()
-    # df = pd.read_csv(fname, comment='#', sep='\t', header=0)
-    # df = pd.read_csv(fname, sep='\t', skiprows=5, header=None, names=cols)  # not working
-    # df = pd.read_csv(fname, sep='\t', skiprows=5, header=0, names=cols)  # not working
-    df = pd.read_csv(fname, sep='\t', skiprows=5, header=None)
-    df.columns = cols + ['\t']
-    df = df.iloc[:, :-1]
+
+# 2.) loading pickled sindy model
+
+import pickle
+
+# model_path = '/home/ffederic/work/Collaboratory/test/experimental_data/functions/'
 
 
-    return df, cols
+
+model_path = f'results/auto-gen/model_VI_untot0_deg1_thr100.pkl'
+model_path = 'results/auto-gen/model_VI_untot0_deg3_thr1.52e-05.pkl'
+model_path = 'results/auto-gen/model_VI_untot0_deg3_thr2e-05.pkl'
+model_path = 'results/auto-gen/model_VI_untot0_deg3_thr1e-06.pkl'
 
 
-print('\n'*5)
-saved_cols = []
-for fname in [fnamei, fnameii, fnameiii]:
-    df, cols = loaded(dir + fnamei)
-    print(f'starting loading {fname}')
-    print(df)
-    print(len(cols), cols)
-    print(saved_cols == cols)
-    saved_cols = cols
-    print('end loading')
-# 1/0
-
-# df = pd.read_csv(dir + fnamei)
-# df = pd.read_csv(dir + fnameie, header=[0,1])
-# df = pd.read_csv(dir + fnameie, sep='\t')
-# df = pd.read_csv(dir + fnameie, sep='\t')
-print(df.columns)
-
-print(df)
-
-df_cut = df[1:]
-print(df_cut)
-a = np.array(df)
-
-print(len(df.index))
-# 1/0
-# for row in df_cut:
-#     print(row)
-print(df[1:2])
-print('df0')
-
-# print(a[1])
-
-
-# for i in df:
-#     print(i)
-
-def check_tot(df):
-    """check tot"""
-
-    print('inside check tot')
-
-    print(len(df.index))
-    count = 0
-    for i in range(0, len(df.index)):
-        # row = df[i:i+1]
-        # print(i, row)
-        row = np.array(df[i:i+1])[0]
-        # row = list(df[i:i+1])
-        # print(i, row)
-
-        # print([i for i in row])
-        # print(row[1], 'data and sum', sum(row[2:]), sum(row[2:])*2, row[1:])
-        print(row[1], 'data and sum', row[1]-sum(row[2:]))
-        if row[1] < sum(row[2:]):
-            count += 1
-            print('\n'*5, 'count', count)
-        # print(row[1], 'sum', sum(row[2:]), sum(row[2:])*2,)
-        # print(row[1], 'explain', [sum(row[2:i]) for i in range(2+1, len(row))])
-        # break
-    print(count, 'count')
-
-    return
-
-check_tot(df_cut)
+with open(model_path, 'rb') as f:
+    model = pickle.load(f)
+model.print(precision=3)
+print(2)
 
--- simple_edit.py
+++ simple_edit.py
@@ -1,19 +1,19 @@
-import numpy as np
-import warnings
-# warnings.filterwarnings("ignore")
-import pysindy as ps
 import datetime
-import warnings
-# warnings.filterwarnings("ignore")
+import os
 
+import pysindy as ps
+import numpy as np
 # print(pysindy.__version__, sys.version)
 # print(warnings.filters)
 
 from loadit import loaded, fnamei, fnameii, fnameiii
 
+MAKEFILE = True
+
 fname = fnamei
 df = loaded(fname)
-# print(df)
+print(df)
+1/0
 
 # t = np.linspace(0, 1, 100)
 t = np.array(df['t'])
@@ -49,41 +49,54 @@ threshold = 2.24
 # threshold = 8
 # threshold = 9
 # threshold = 10
-# threshold = 100
-# threshold = 1e-1
+threshold = 100
+# # threshold = 1e-1
+# threshold = 5e-2
+# threshold = 4e-2
+# threshold = 3e-2
+# threshold = 2.5e-2
+# threshold = 2e-2
 # threshold = 1e-2
-threshold = 2e-2
 # threshold = 0.001
 # threshold = 0.0012005   # good
 # threshold = 0.0013005   # good
-
-# threshold = 0.0014
-# threshold = 0.00146
-# threshold = 0.00146104  # good
-# threshold = 0.00146504  # good
-# threshold = 0.00146604  # good
-# threshold = 0.0014665  # bad
-# threshold = 0.00146704  # bad
-# threshold = 0.00146804  # bad
-# threshold = 0.00147
-# threshold = 0.0015005   # zero
-
+#
+# # threshold = 0.0014
+# # threshold = 0.00146
+# # threshold = 0.00146104  # good
+# # threshold = 0.00146504  # good
+# # threshold = 0.00146604  # good
+# # threshold = 0.0014665  # bad
+# # threshold = 0.00146704  # bad
+# # threshold = 0.00146804  # bad
+# # threshold = 0.00147
+# # threshold = 0.0015005   # zero
+#
 # threshold = 1e-3  # interesting output
-# threshold = 1e-4
-# threshold = 1e-5
+threshold = 1e-4
+# threshold = 2e-5
+# threshold = 1.52e-5
+threshold = 1e-5
+# threshold = 1e-6
+# # threshold = 1e-7
 # threshold = 1e-8
 # threshold = 1e-9
+
 # # threshold = 1.5e-10
-# threshold = 2e-10
-# threshold = 3e-10
-# threshold = 5e-10
+# # threshold = 2e-10
+# # threshold = 3e-10
+# # threshold = 5e-10
 # threshold = 1e-10
+# threshold = 1e-12
 # threshold = 1e-15
 
+degree = 1
+# degree = 2
+degree = 3
 # model = ps.SINDy(feature_names=["x", "y"],
 #                 optimizer = ps.STLSQ(threshold=threshold))
 model = ps.SINDy(feature_names=df.columns[1+shift:scale],
-                 # feature_library=ps.PolynomialLibrary(degree=1),
+                 feature_library=ps.PolynomialLibrary(degree=degree),
                  optimizer = ps.STLSQ(threshold=threshold))
 
 model.fit(X, t=t)
@@ -91,10 +104,41 @@ model.fit(X, t=t)
 # print(model.coefficients())
 print(sum(abs(model.coefficients()[0, :])>0))
 
-print(f'\nfilename: {fname}, threshold: {threshold}, date: {print(datetime.datetime.now())} \n')
-model.print()
+total = False if shift == 1 else True
+meta = f'\nfilename: {fname}, \'total\' included: {total}, degree: {degree}, threshold: {threshold}, date: {str(datetime.datetime.now())} \n'
+print(meta)
 
-print(sum(abs(model.coefficients()[0, :])>0))
+from io import StringIO
+import sys
+tmp = sys.stdout
+my_result = StringIO()
+sys.stdout = my_result
+# print('hello world')  # output stored in my_result
+model.print(precision=4)
+sys.stdout = tmp
+eqs = my_result.getvalue()
+print(eqs)
+
+
+# print(sum(abs(model.coefficients()[0, :])>0))
+
+if MAKEFILE:
+
+    vi = fname.split('_')[3]
+    outdir = f'results{os.sep}auto-gen{os.sep}'
+    outtxt = f'res_{vi}_{"un"*(not total)}tot{int(total)}_deg{degree}_thr{threshold}.txt'
+    outpath = outdir + outtxt
+    print(f'results written to {outpath}!!')
+    f = open(outpath, 'w')
+    f.write(meta + '\n')
+    f.write(eqs)
+    f.close()
+
+    import pickle
+    outmodel = f'{outdir}model{outtxt[3:-4]}.pkl'
+    with open(outmodel, 'wb') as f:
+        pickle.dump(model, f)
+    print(f'model_path = \'{outmodel}\'')
 
 # model.print(lhs=["total"])
 
