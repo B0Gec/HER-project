f375335f501ed75e479bf9bd17d0c65af4f74bc9
bogec <bostogec@gmail.com>
2023-08-31 16:36:48 +0200

    vi end of running maybe

--- simple_edit.py
+++ simple_edit.py
@@ -87,7 +87,7 @@ threshold = 3
 # # threshold = 1e-4
 # # # threshold = 2e-5
 # # # threshold = 1.52e-5
-# # # threshold = 1e-5
+# threshold = 1e-5
 # # # # threshold = 1e-6
 # # # # # threshold = 1e-7
 # # # # threshold = 1e-8
@@ -103,65 +103,85 @@ threshold = 3
 
 degree = 1
 # degree = 2
-# degree = 3
+degree = 3
 # model = ps.SINDy(feature_names=["x", "y"],
 #                 optimizer = ps.STLSQ(threshold=threshold))
 model = ps.SINDy(feature_names=df.columns[1+shift:scale],
                  feature_library=ps.PolynomialLibrary(degree=degree),
                  optimizer = ps.STLSQ(threshold=threshold))
 
-model.fit(X, t=t)
+main = True
+main = False
+if main:
+    model.fit(X, t=t)
 
-# print(model.coefficients())
-print(sum(abs(model.coefficients()[0, :])>0))
+    # print(model.coefficients())
+    print(sum(abs(model.coefficients()[0, :])>0))
 
-total = False if shift == 1 else True
-meta = f'\nfilename: {fname}, \'total\' included: {total}, degree: {degree}, threshold: {threshold}, date: {str(datetime.datetime.now())} \n'
-print(meta)
+    total = False if shift == 1 else True
+    precision = 3
+    meta = f'\n% filename: {fname}, \'total\' included: {total}, degree: {degree}, threshold: {threshold}, date: {str(datetime.datetime.now())} \n'
+    print(meta)
 
-from io import StringIO
-import sys
-tmp = sys.stdout
-my_result = StringIO()
-sys.stdout = my_result
-# print('hello world')  # output stored in my_result
-model.print(precision=4)
-sys.stdout = tmp
-eqs = my_result.getvalue()
-print(eqs)
+    latex1 = f"""
+    \subsection{{Scenario with degree {degree}, threshold {threshold} and precision {precision}}}
 
-print(meta)
+    Ordinary differential equations (ODEs) outputed by SINDy:
 
+    \\begin{{verbatim}}"""
+    print(latex1)
 
-x_train = X
-# x_test = x_train[:100, :]
-x_test = x_train
-t_test = t
-dt = np.mean(t[1:] - t[:-1])
-# rmse = mean_squared_error(x_train, np.zeros(x_train.shape), squared=False)
-mse = model.score(x_test, t=dt, metric=mean_squared_error)
+    from io import StringIO
+    import sys
+    tmp = sys.stdout
+    my_result = StringIO()
+    sys.stdout = my_result
+    # print('hello world')  # output stored in my_result
+    model.print(precision=precision)
+    sys.stdout = tmp
+    eqs = my_result.getvalue()
+    print(eqs)
 
+    latex2 = """\end{verbatim}
 
-# print('mse lstsq', i, 'done')
-sim_success = True
-try:
-    x_test_sim = model.simulate(x_test[0, :], t_test, integrator="odeint")
-except:
-    sim_success = False
-    mse_sim = np.inf
-if sim_success:
-    if np.any(x_test_sim > 1e4):
-        x_test_sim = 1e4
-    mse_sim = np.mean((x_test - x_test_sim) ** 2)
+    """
 
-printmses = f'mse: {mse:.4e}, mse_sim: {mse_sim:.4e}'
-print(printmses)
-
-# print(sum(abs(model.coefficients()[0, :])>0))
+    print(latex2)
 
 
+    x_train = X
+    # x_test = x_train[:100, :]
+    x_test = x_train
+    t_test = t
+    dt = np.mean(t[1:] - t[:-1])
+    # rmse = mean_squared_error(x_train, np.zeros(x_train.shape), squared=False)
+    mse = model.score(x_test, t=dt, metric=mean_squared_error)
 
-# MAKEFILE = False
+
+    # print('mse lstsq', i, 'done')
+    sim_success = True
+    try:
+        x_test_sim = model.simulate(x_test[0, :], t_test, integrator="odeint")
+    except:
+        sim_success = False
+        mse_sim = np.inf
+    if sim_success:
+        if np.any(x_test_sim > 1e4):
+            x_test_sim = 1e4
+        mse_sim = np.mean((x_test - x_test_sim) ** 2)
+
+    printmses = f'mse: {mse:.4e}, mse_sim: {mse_sim:.4e}'
+    printmses = f"""
+    with: \\
+        \\verb| mse: {mse:.4e}, mse_sim: {mse_sim:.4e} |\n\n
+    """
+    print(printmses)
+
+    # print(sum(abs(model.coefficients()[0, :])>0))
+
+
+
+MAKEFILE = False
 if MAKEFILE:
 
     vi = fname.split('_')[3]
@@ -194,101 +214,147 @@ if MAKEFILE:
 
 
 hyperparametertuning = True
-hyperparametertuning = False
+# hyperparametertuning = False
 if hyperparametertuning:
 
-    from scipy.integrate import solve_ivp
-    from pysindy.utils import lorenz, lorenz_control, enzyme
+    lorenz = True
+    lorenz = False
+    if lorenz:
+        from scipy.integrate import solve_ivp
+        from pysindy.utils import lorenz, lorenz_control, enzyme
 
-    np.random.seed(100)
+        np.random.seed(100)
 
-    # Initialize integrator keywords for solve_ivp to replicate the odeint defaults
-    integrator_keywords = {}
-    integrator_keywords['rtol'] = 1e-12
-    integrator_keywords['method'] = 'LSODA'
-    integrator_keywords['atol'] = 1e-12
+        # Initialize integrator keywords for solve_ivp to replicate the odeint defaults
+        integrator_keywords = {}
+        integrator_keywords['rtol'] = 1e-12
+        integrator_keywords['method'] = 'LSODA'
+        integrator_keywords['atol'] = 1e-12
 
-    dt = 0.002
+        dt = 0.002
 
-    t_train = np.arange(0, 10, dt)
-    x0_train = [-8, 8, 27]
-    t_train_span = (t_train[0], t_train[-1])
-    x_train = solve_ivp(
-        lorenz, t_train_span, x0_train, t_eval=t_train, **integrator_keywords
-    ).y.T
+        t_train = np.arange(0, 10, dt)
+        x0_train = [-8, 8, 27]
+        t_train_span = (t_train[0], t_train[-1])
+        x_train = solve_ivp(
+            lorenz, t_train_span, x0_train, t_eval=t_train, **integrator_keywords
+        ).y.T
 
-    t_test = np.arange(0, 15, dt)
-    t_test_span = (t_test[0], t_test[-1])
-    x0_test = np.array([8, 7, 15])
-    x_test = solve_ivp(
-        lorenz, t_test_span, x0_test, t_eval=t_test, **integrator_keywords
-    ).y.T
+        t_test = np.arange(0, 15, dt)
+        t_test_span = (t_test[0], t_test[-1])
+        x0_test = np.array([8, 7, 15])
+        x_test = solve_ivp(
+            lorenz, t_test_span, x0_test, t_eval=t_test, **integrator_keywords
+        ).y.T
 
-    # print(x_train.shape, t_train.shape, x_test.shape, t_test.shape)
-    # 1/0
-    # Instantiate and fit the SINDy model
-    feature_names = ['x', 'y', 'z']
-    sparse_regression_optimizer = ps.STLSQ(threshold=0)  # default is lambda = 0.1
-    model = ps.SINDy(feature_names=feature_names, optimizer=sparse_regression_optimizer)
-    model.fit(x_train, t=dt)
-    model.print()
+        # print(x_train.shape, t_train.shape, x_test.shape, t_test.shape)
+        # 1/0
+        # Instantiate and fit the SINDy model
+        feature_names = ['x', 'y', 'z']
+        sparse_regression_optimizer = ps.STLSQ(threshold=0)  # default is lambda = 0.1
+        model = ps.SINDy(feature_names=feature_names, optimizer=sparse_regression_optimizer)
+        model.fit(x_train, t=dt)
+        model.print()
 
-    # Make coefficient plot for threshold scan
+        # Make coefficient plot for threshold scan
 
-
-    def plot_pareto(coefs, opt, model, threshold_scan, x_test, t_test):
+    MAKEFILE = True
+    def plot_pareto(coefs, opt, model, threshold_scan, x_test, t_test, degree):
         dt = t_test[1] - t_test[0]
         mse = np.zeros(len(threshold_scan))
         mse_sim = np.zeros(len(threshold_scan))
-        for i in range(len(threshold_scan)):
+        skipsim = True
+        skipsim = False
+        tuning = ""
+        title = 'degree | threshold | stslq mse | sim mse\n'
+        if MAKEFILE:
+            vi = fname.split('_')[3]
+            outdir = f'results{os.sep}auto-gen{os.sep}'
+            outtxt = f'tuning_{vi}_deg{degree}.txt'
+            outpath = outdir + outtxt
+            print(f'tuning table written to {outpath}!!')
+            print('\n\ntitle', title)
+            with open(outpath, 'w') as f:
+                f.write(title)
+
+        for i, threshold in enumerate(threshold_scan):
             opt.coef_ = coefs[i]
             mse[i] = model.score(x_test, t=dt, metric=mean_squared_error)
-            print('mse lstsq', i, 'done')
+            print(f'mse lstsq {i} done, mse[{i}]: {mse[i]}')
             sim_success = True
-            try:
-                x_test_sim = model.simulate(x_test[0, :], t_test, integrator="odeint")
-            except:
-                sim_success = False
-                mse_sim[i] = np.inf
-            if sim_success:
-                if np.any(x_test_sim > 1e4):
-                    x_test_sim = 1e4
-                mse_sim[i] = np.sum((x_test - x_test_sim) ** 2)
-            else:
-                mse_sim[i] = np.inf
-            print('mse sim', i, 'done')
+            if not skipsim:
+                try:
+                    x_test_sim = model.simulate(x_test[0, :], t_test, integrator="odeint")
+                except:
+                    sim_success = False
+                    mse_sim[i] = np.inf
+                if sim_success:
+                    if np.any(x_test_sim > 1e4):
+                        x_test_sim = 1e4
+                    mse_sim[i] = np.sum((x_test - x_test_sim) ** 2)
+                else:
+                    mse_sim[i] = np.inf
+                print(f'mse sim {i} done, mse_sim[{i}]: {mse_sim[i]}')
+
+            tuning = f'% {degree} & {threshold:.4e} & {mse[i]:.4e}  & {mse_sim[i]:.4e}  \\\\'
+            if MAKEFILE:
+                print(f'tuning table written to {outpath}!!')
+                with open(outpath, 'a') as f:
+                    f.write(tuning + '\n')
+
 
         plt.figure()
         plt.semilogy(threshold_scan, mse, "bo")
         plt.semilogy(threshold_scan, mse, "b")
-        time = str(datetime.datetime.now()) + 'mse lstsq'
+        time = str(datetime.datetime.now()) + f'mse lstsq deg {degree}'
         plt.title(time, fontsize=20)
         plt.ylabel(r"$\dot{X}$ RMSE", fontsize=20)
         plt.xlabel(r"$\lambda$ 0", fontsize=20)
         plt.xticks(fontsize=16)
         plt.yticks(fontsize=16)
         plt.grid(True)
+        # MAKEFILE = False
+        if MAKEFILE:
+            outpng = f'tuning_{vi}_deg{degree}_mse.png'
+            outpngpath = outdir + outpng
+            plt.savefig(outpngpath, dpi=300)
+            print(f'tunning pic written to {outpngpath}!!')
 
-        plt.figure()
-        plt.semilogy(threshold_scan, mse_sim, "bo")
-        plt.semilogy(threshold_scan, mse_sim, "b")
-        time = str(datetime.datetime.now()) + 'mse sim'
-        plt.title(time, fontsize=20)
-        plt.ylabel(r"$\dot{X}$ RMSE sim", fontsize=20)
-        plt.xlabel(r"$\lambda$ sim", fontsize=20)
-        plt.xticks(fontsize=16)
-        plt.yticks(fontsize=16)
-        plt.grid(True)
-        print('here i')
-        plt.show()
+        if not skipsim:
+            plt.figure()
+            plt.semilogy(threshold_scan, mse_sim, "bo")
+            plt.semilogy(threshold_scan, mse_sim, "b")
+            time = str(datetime.datetime.now()) + f'mse sim deg {degree}'
+            plt.title(time, fontsize=20)
+            plt.ylabel(r"$\dot{X}$ RMSE sim", fontsize=20)
+            plt.xlabel(r"$\lambda$ sim", fontsize=20)
+            plt.xticks(fontsize=16)
+            plt.yticks(fontsize=16)
+            plt.grid(True)
+            outpngpath = outdir + outpng[:-4] + '_sim.png'
+            plt.savefig(outpngpath, dpi=300)
+            print(f'tunning sim pic written to {outpngpath}!!')
 
+    plt.show()
     # plot_pareto()
 
 
-    # threshold_scan = np.linspace(0, 1.0, 10)[:2002]
-    threshold_scan = np.linspace(0.9, 1, 10)[:2002]
+    threshold_scan = np.linspace(0, 1.0, 10)[:2002]
+    # threshold_scan = np.linspace(0.9, 1, 10)[:2002]
     threshold_scan = np.linspace(0.0, 0.1, 10)[:2002]
+    threshold_scan = [0, 1.52e-05, 1e-05, 1e-15  ]
     threshold_scan = np.linspace(0.0, 0.000000001, 4)[:2002]
+    threshold_scan_std = np.linspace(0, 1.0, 10)[:2002]
+    # deg3 = [10**(i) for i in range(-9, -2)]
+    deg2 = list(threshold_scan_std) + [0.1, 0.03, 0.05, 0.025, ]
+    deg3 = list(threshold_scan_std)[3:] + [1e-05, 1.52e-05, 1e-15, ]
+    # deg2 = deg2[:3]
+    # deg3 = deg3[:3]
+    # thr_std = [(3, thr) for thr in threshold_scan_std] + [(2, thr) for thr in threshold_scan_std]
+    # degthr = deg3 + deg2
+    # degthr = deg2
+    # degthr = deg3
+
     # threshold_scan = np.linspace(0.922, 0.924, 50)[:2002]
     coefs = []
     x_train = X
@@ -298,31 +364,41 @@ if hyperparametertuning:
     dt = np.mean(t[1:] - t[:-1])
     rmse = mean_squared_error(x_train, np.zeros(x_train.shape), squared=False)
     # x_train_added_noise = x_train + np.random.normal(0, rmse / 10.0,  x_train.shape)
+
     x_train_added_noise = x_train
-    for i, threshold in enumerate(threshold_scan):
-        # Instantiate and fit the SINDy model
-        feature_names = ['x', 'y', 'z']
-        # sparse_regression_optimizer = ps.STLSQ(threshold=0)  # default is lambda = 0.1
-        # model.fit(x_train, t=dt)
+    for degree, threshold_scan in [(2, deg2), (3, deg3)][1:2]:
+        # degree = deg + 2
+        for i, threshold in enumerate(threshold_scan):
+            # print('inside loop')
+            # print(config)
+            # Instantiate and fit the SINDy model
+            feature_names = ['x', 'y', 'z']
+            # sparse_regression_optimizer = ps.STLSQ(threshold=0)  # default is lambda = 0.1
+            # model.fit(x_train, t=dt)
 
-        sparse_regression_optimizer = ps.STLSQ(threshold=threshold)
-        # model = ps.SINDy(feature_names=feature_names, optimizer=sparse_regression_optimizer)
-        model = ps.SINDy(feature_names=df.columns[1 + shift:scale],
-                         feature_library=ps.PolynomialLibrary(degree=degree),
-                         optimizer=sparse_regression_optimizer)
-        # model = ps.SINDy(feature_names=feature_names,
-        #                  optimizer=sparse_regression_optimizer)
-        model.fit(x_train_added_noise, t=dt, quiet=True)
-        print(f'fitted: {i}, with thr: {threshold}')
-
-
-        coefs.append(model.coefficients())
-
-    print('after loop')
-    plot_pareto(coefs, sparse_regression_optimizer, model,
-                threshold_scan, x_test, t_test)
+            # degree, threshold = config
+            sparse_regression_optimizer = ps.STLSQ(threshold=threshold)
+            # model = ps.SINDy(feature_names=feature_names, optimizer=sparse_regression_optimizer)
+            model = ps.SINDy(feature_names=df.columns[1 + shift:scale],
+                             feature_library=ps.PolynomialLibrary(degree=degree),
+                             optimizer=sparse_regression_optimizer)
+            # model = ps.SINDy(feature_names=feature_names,
+            #                  optimizer=sparse_regression_optimizer)
+            model.fit(x_train_added_noise, t=dt, quiet=True)
+            print(f'fitted: {i}, with thr: {degree, threshold}')
 
 
-    plt.plot(t, t, 'k--')
+            coefs.append(model.coefficients())
+
+        # print('after loop')
+        # threshold_scan = [thr for deg, thr in degthr]
+        plot_pareto(coefs, sparse_regression_optimizer, model,
+                    threshold_scan, x_test, t_test, degree)
+        plot_pareto(coefs, sparse_regression_optimizer, model,
+                    threshold_scan, x_test, t_test, degree)
+
+
+    # plt.plot(t, t, 'k--')
     # 4 * 10^4 lstmse at 0.923
     # 1 * 10^4 lstmse at 0
+
